b8a5be3c5ad1464d4c96e171512dce75b7ca0c5c
cetalogy <cetalogy.id@gmail.com>
2023-11-21 20:41:46 +0700

    First

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Initialize the Gin router
+	router := gin.Default()
+
+	// Define a GET route to handle "/"
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// Run the server on port 8080
+	router.Run(":8080")
+}
